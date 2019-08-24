48aadf56b1142627fc899b70f0fd3e8b4ed7f7c4
kwr760 <kroy760@gmail.com>
2019-08-23 21:17:51 -0700

    pass linting

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,41 +1,38 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-process.env.NODE_ENV = "development";
+process.env.NODE_ENV = 'development';
 
 module.exports = {
-  mode: "development",
-  target: "web",
-  devtool: "cheap-module-source-map",
-  entry: "./src/web/index",
+  mode: 'development',
+  target: 'web',
+  devtool: 'cheap-module-source-map',
+  entry: './src/web/index',
   output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
   },
   devServer: {
-    stats: "minimal",
+    stats: 'minimal',
     overlay: true,
     historyApiFallback: true,
     disableHostCheck: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    https: false
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    https: false,
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.API_URL": JSON.stringify("http://localhost:3000")
+      'process.env.API_URL': JSON.stringify('http://localhost:3000'),
     }),
     new HtmlWebpackPlugin({
-      template: "src/web/index.html",
-      favicon: "src/web/favicon.ico"
-    })
+      template: 'src/web/index.html',
+      favicon: 'src/web/favicon.ico',
+    }),
   ],
   resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ]
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
@@ -47,24 +44,18 @@ module.exports = {
             loader: 'babel-loader',
             options: {
               babelrc: false,
-              presets: ['@babel/preset-react', '@babel/preset-env']
-            }
-            // options: {
-            //   presets: ['react']
-            // }
+              presets: ['@babel/preset-react', '@babel/preset-env'],
+            },
           },
           {
             loader: 'eslint-loader',
           },
-        ]
+        ],
       },
       {
         test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
