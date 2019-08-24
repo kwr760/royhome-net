8af7300a328578de3d39f0e769ba41a920f6fb95
kwr760 <kroy760@gmail.com>
2019-08-23 21:54:34 -0700

    clean up webpack

--- src/web/components/App.js
+++ src/web/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className="container-fluid">Kevin is here</div>;
+  return <div className="container-fluid">Kevin Roy is here</div>;
 }
 
 export default App;
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,35 +2,39 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 process.env.NODE_ENV = 'development';
 
 module.exports = {
   mode: 'development',
-  target: 'web',
-  devtool: 'cheap-module-source-map',
-  entry: './src/web/index',
+  // target: 'web',
+  devtool: 'inline-source-map',
+  entry: './src/web/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.API_URL': JSON.stringify('http://localhost:3000'),
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/web', 'index.html'),
+      favicon: path.resolve(__dirname, 'src/web', 'favicon.ico'),
+      filename: './index.html',
+    }),
+  ],
   devServer: {
     stats: 'minimal',
     overlay: true,
     historyApiFallback: true,
     disableHostCheck: true,
+    contentBase: './dist',
     headers: { 'Access-Control-Allow-Origin': '*' },
     https: false,
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.API_URL': JSON.stringify('http://localhost:3000'),
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/web/index.html',
-      favicon: 'src/web/favicon.ico',
-    }),
-  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
