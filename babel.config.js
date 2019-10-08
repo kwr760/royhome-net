c55c24677e3073b31cde464b65677b87949712c7
kwr760 <kroy760@gmail.com>
2019-10-07 20:42:47 -0700

    Add babel config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,61 @@
 // babel.config.js
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
+    'lodash',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
   env: {
-    test: {
+    production: {
+      presets: [
+        [
+          '@babel/env',
+          {
+            modules: false,
+            useBuiltIns: 'entry',
+            corejs: { version: '3' },
+            targets: { browsers: 'last 2 versions' },
+          },
+        ],
+        '@babel/react',
+      ],
+      retainLines: true,
       plugins: [
-        'transform-es2015-modules-commonjs',
+        'lodash',
+        '@babel/plugin-transform-runtime',
+        '@loadable/babel-plugin',
+        '@babel/plugin-syntax-dynamic-import',
+      ],
+    },
+    development: {
+      presets: [
+        [
+          '@babel/env',
+          {
+            modules: false,
+            useBuiltIns: 'entry',
+            corejs: { version: '3' },
+            targets: { browsers: 'last 2 versions' },
+          },
+        ],
+        '@babel/react',
+      ],
+      retainLines: true,
+      plugins: [
+        'lodash',
+        '@babel/plugin-transform-runtime',
+        '@loadable/babel-plugin',
+        '@babel/plugin-syntax-dynamic-import',
+      ],
+    },
+    test: {
+      presets: ['@babel/env', '@babel/react'],
+      retainLines: true,
+      plugins: [
+        'lodash',
+        '@babel/plugin-transform-runtime',
+        'dynamic-import-node-babel-7',
+        '@loadable/babel-plugin',
+        '@babel/plugin-syntax-dynamic-import',
       ],
     },
   },
