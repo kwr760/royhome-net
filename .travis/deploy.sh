a420709890d2a295e28d82575ccf86d9d50e2414
kwr760 <kroy760@gmail.com>
2019-10-23 20:48:25 -0700

    Test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,15 +1,16 @@
 echo "The deploy script has run!"
 
 echo $RELEASE_IP_ADDR
+echo $RELEASE_PASSWORD
+echo $GH_USER
+echo $GH_PASS
 
-ping 45.79.110.249
+sshpass -p $RELEASE_PASSWORD ssh $RELEASE_IP_ADDR ls
 
-ssh server@45.79.110.249
-
-git clone https://${GH_USER}:${GH_PASS}@github.com/kwr760/royhome-net.git /var/www/royhome.net
-cd /var/www/royhome.net
-yarn install
-yarn run build:prod
-yarn run start:prod
+#git clone https://${GH_USER}:${GH_PASS}@github.com/kwr760/royhome-net.git /var/www/royhome.net
+#cd /var/www/royhome.net
+#yarn install
+#yarn run build:prod
+#yarn run start:prod
 
 
