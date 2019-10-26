6d9609106f25a6748be1d267bd643b2d0b30b0a7
kwr760 <kroy760@gmail.com>
2019-10-26 15:38:46 -0700

    Update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,12 +1,21 @@
 echo "The deploy script has run!"
 
-echo $RELEASE_IP_ADDR
-echo $RELEASE_PASSWORD
-echo $GH_USER
-echo $GH_PASS
+eval "$(ssh-agent -s)"
 
-apt install sshpass
-sshpass -p $RELEASE_PASSWORD ssh $RELEASE_IP_ADDR ls
+ssh-add <(echo "$SSH_PRIVATE_KEY")
+
+# List out your new key's fingerprint
+ssh-add -l
+
+ssh $RELEASE_HOST  ls -la
+ssh $RELEASE_HOST  rm -rf /var/app/royhome-net.sav
+ssh $RELEASE_HOST  mv /var/app/royhome-net /var/app/royhome-net.sav
+ssh $RELEASE_HOST  git clone https://$GITHUB_LOGIN@github.com/kwr760/royhome-net.git /var/app
+
+# Don't forget to cleanup your agent after you're done using it if you're not on an ephemeral build server.
+ssh-agent -k
+
+ssh-add -l
 
 #git clone https://${GH_USER}:${GH_PASS}@github.com/kwr760/royhome-net.git /var/www/royhome.net
 #cd /var/www/royhome.net
