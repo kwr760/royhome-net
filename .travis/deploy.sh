4201e977770cf3cf55aaa8b69db4bea24bbdba80
kwr760 <kroy760@gmail.com>
2019-10-27 08:49:36 -0700

    Rename

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,7 @@ echo "The deploy script has run!"
 
 eval "$(ssh-agent -s)"
 
-openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/private-keY.enc -out .travis/private-keY -d
+openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/private-key.enc -out .travis/private-key -d
 chmod 600 .travis/private-key
 ssh-add .travis/private-key
 rm .travis/private-key
