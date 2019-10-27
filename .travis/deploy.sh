605b102656e25d193edfa3d122b3605a5a9b112e
kwr760 <kroy760@gmail.com>
2019-10-27 14:24:21 -0700

    TEst

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,14 +6,10 @@ chmod 600 .travis/private-key
 ssh-add .travis/private-key
 rm .travis/private-key
 
-# List out your new key's fingerprint
-ssh-add -l
-
-echo $(date +%F_%T)
-
 ssh $RELEASE_HOST  mv /var/app/royhome-net.last /var/app/royhome-net.$(date +%F_%T)
 ssh $RELEASE_HOST  mv /var/app/royhome-net /var/app/royhome-net.last
-ssh $RELEASE_HOST  git clone git@github.com:kwr760/royhome-net.git /var/app/2
+ssh $RELEASE_HOST  git clone git@github.com:kwr760/royhome-net.git /var/app/royhome-net.git
+ssh $RELEASE_HOST  git clone https://github.com/kwr760/royhome-net.git /var/app/royhome-net.https
 ssh $RELEASE_HOST  ls -la /var/app
 
 # Don't forget to cleanup your agent after you're done using it if you're not on an ephemeral build server.
