5bea8142c606a2700f16bf57f1b1454480e4c8bc
kwr760 <kroy760@gmail.com>
2019-10-27 13:24:42 -0700

    Test:

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,6 @@ eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/private-key.enc -out .travis/private-key -d
 chmod 600 .travis/private-key
 ssh-add .travis/private-key
-cat .travis/private-key
 rm .travis/private-key
 
 # List out your new key's fingerprint
