e2167a301a3d98382249f41db6e90c4e45dbef1f
kwr760 <kroy760@gmail.com>
2019-10-27 14:46:28 -0700

    Only deploy on master push

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
-echo "The deploy script has run!"
+echo "Deploy the latest code"
 
 eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/private-key.enc -out .travis/private-key -d
