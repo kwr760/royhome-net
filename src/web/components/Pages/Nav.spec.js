b574300625285debd36e4fced2aa331b36461885
kwr760 <kroy760@gmail.com>
2019-11-09 11:36:47 -0800

    Add unit test

---
+++ src/web/components/Pages/Nav.spec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import Nav from './Nav';
+import AuthContext from '../Auth/AuthContext';
+
+jest.mock('../Auth/Auth');
+
+describe('web/components/Pages/Nav', () => {
+  it('should render with authentication and role', () => {
+    // Arrange
+    const auth = {
+      isAuthenticated: jest.fn(() => true),
+      login: jest.fn(),
+      logout: jest.fn(),
+      userHasRole: jest.fn(() => true),
+    };
+
+    // Act
+    const { getByText } = render(
+      <Router>
+        <AuthContext.Provider value={auth}>
+          <Nav />
+        </AuthContext.Provider>
+      </Router>,
+    );
+
+    // Assert
+    getByText(/Kevin/);
+    getByText(/Resume/);
+    getByText(/Profile/);
+    getByText(/Private/);
+    getByText(/Courses/);
+    getByText(/Log Out/);
+  });
+});
