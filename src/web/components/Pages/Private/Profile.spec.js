0094b690db8aadee0a9e7096cb8fcbbff980d13b
kwr760 <kroy760@gmail.com>
2019-11-10 08:40:52 -0800

    Unit Tests

---
+++ src/web/components/Pages/Private/Profile.spec.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, waitForElement } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Profile from './Profile';
+
+describe('web/components/Pages/Private/Profile', () => {
+  it('should render fetched message', async () => {
+    // Arrange
+    const error = undefined;
+    const profile = {
+      nickname: 'Nickname',
+      picture: 'picture',
+      arg: 'Loaded Arg',
+    };
+
+    const auth = {
+      getAccessToken: jest.fn(),
+      getProfile: jest.fn((cb) => cb(profile, error)),
+    };
+
+    // Arrange
+    const { getByText } = render(<Profile auth={auth} />);
+
+    await waitForElement(() => getByText('Profile'));
+    getByText('Nickname');
+    getByText(/picture/);
+    getByText(/Loaded Arg/);
+  });
+});
