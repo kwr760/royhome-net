b33f059133c32b54fb1e2c1d0dfc9ef864160956
Kevin Roy <kroy760@gmail.com>
2019-11-24 21:31:43 -0800

    Web 40 (#28)
    * WEB-40 Travis build is not running tests correctly
    
    * WEB-40 Travis build is not running tests correctly

---
+++ src/server/logger/log.js
@@ -0,0 +1,25 @@
+// import env from '../../config';
+
+// import getConsole from '../../util/logger/get-console';
+// import formatMessage from '../../util/logger/format-message';
+// import getLogFilename from './get-filename';
+// import writeToLog from './write-to-log';
+
+// const log = ({ level, msg }) => {
+const log = () => {
+  // const { dir: logLocation, level: logLevel, stdout: displayToScreen } = env.log;
+  //
+  // if (level >= logLevel) {
+  //   const logMessage = formatMessage(level, msg);
+  //   const logFile = getLogFilename(logLocation);
+  //
+  //   writeToLog(logFile, logMessage);
+  //
+  //   if (displayToScreen) {
+  //     const consoleCb = getConsole(level);
+  //     consoleCb(logMessage);
+  //   }
+  // }
+};
+
+export default log;
--- src/server/middleware/handle-error.js
+++ src/server/middleware/handle-error.js
@@ -1,7 +1,8 @@
 import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
+import Logger from '../logger';
 
 const handleError = (err, req, res) => {
-  console.error(err.message);
+  Logger.error(err.message);
   return res.sendStatus(INTERNAL_SERVER_ERROR);
 };
 
