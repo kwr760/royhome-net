39ccc004f6c95dfc5463372e2afab379dcb1d929
Kevin Roy <kroy@expediagroup.com>
2019-12-26 02:30:31 -0800

    WEB-17 - Merge conflicts

--- src/server/middleware/start-https.js
+++ src/server/middleware/start-https.js
@@ -3,11 +3,7 @@ import https from 'https';
 import displayMessage from './display-message';
 import env from '../../config';
 
-<<<<<<< HEAD
-const startHttpServer = (app, port) => {
-=======
 const startHttpsServer = (app, port) => {
->>>>>>> 73f56fd7a23017333e4111d7da0ba7827de3b4e9
   const privateKey = fs.readFileSync(env.server.key, 'utf8');
   const certificate = fs.readFileSync(env.server.cert, 'utf8');
   const ca = fs.readFileSync(env.server.ca, 'utf8');
@@ -22,8 +18,4 @@ const startHttpsServer = (app, port) => {
   server.listen(port, displayMessage('Secure server is running'));
 };
 
-<<<<<<< HEAD
-export default startHttpServer;
-=======
 export default startHttpsServer;
->>>>>>> 73f56fd7a23017333e4111d7da0ba7827de3b4e9
