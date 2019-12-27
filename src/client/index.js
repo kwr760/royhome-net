981300d0b67aba0a9c47070d1fe43a8dff75cc27
Kevin Roy <kroy760@gmail.com>
2019-12-26 16:40:01 -0800

    Web 41 (#45)
    * WEB-41 Move web to client
    
    * WEB-41 Move web to client
    
    * WEB-41 Move web to client
    
    * WEB-41 Refactor
    
    * WEB-41 - Refactor - change index.css name

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import './components/App.css';
 import App from './components/App';
 
 render(
