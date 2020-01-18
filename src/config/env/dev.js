9db01609ffd5a90e4680e74b21ec988ced9de2c8
kwr760 <kroy760@gmail.com>
2020-01-18 15:34:42 -0800

    WEB-43 use 3000 and not 3001

--- src/config/env/dev.js
+++ src/config/env/dev.js
@@ -1,6 +1,6 @@
 import LOG_LEVELS from '../../util/logger/levels';
 
-const currentHost = 'http://localhost:3001';
+const currentHost = 'http://localhost:3000';
 
 const env = (host) => ({
   mode: 'development',
