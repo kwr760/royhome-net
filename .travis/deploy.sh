51b961f1c2a5aa117ba091636a6bf275de6605bd
Kevin Roy <kroy760@gmail.com>
2020-02-02 15:20:58 -0800

    Web 42 (#62)
    * WEB-42 Clean comments
    
    * WEB-42 Reorder imports
    
    * WEB-42 Move to dependency
    
    * WEB-42 Load data on server and ship to client
    
    * Load from file
    
    * WEB-42 Load from file
    
    * WEB-42 Add fetchData on route
    
    * WEB-42 Rename router and add prop types
    
    * WEB-42 Add indent
    
    * WEB-42 Add new function
    
    * WEB-42 Improve prop types
    
    * WEB-42 Fix up file
    
    * WEB-42 Fix unit test
    
    * WEB-42 Add unit tests
    
    * WEB-42 load server side jwt
    
    * WEB-42 fix unit tests
    
    * WEB-42 fix unit tests
    
    * Rename icons
    
    * Add certbot renew

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,14 +6,14 @@ chmod 600 .travis/private-key
 ssh-add .travis/private-key
 rm .travis/private-key
 
-ssh $RELEASE_HOST  mv /var/app/royhome-net.last /var/app/royhome-net.$(date +%F_%T)
-ssh $RELEASE_HOST  'cd /var/app/royhome-net ; sudo yarn run pm2:stop'
-ssh $RELEASE_HOST  mv /var/app/royhome-net /var/app/royhome-net.last
-ssh $RELEASE_HOST  git clone https://github.com/kwr760/royhome-net.git /var/app/royhome-net
-ssh $RELEASE_HOST  'cd /var/app/royhome-net ; yarn install'
-ssh $RELEASE_HOST  'cd /var/app/royhome-net ; yarn run build:prod'
-ssh $RELEASE_HOST  'cd /var/app/royhome-net ; sudo yarn run pm2:start'
-ssh $RELEASE_HOST  ls -la /var/app
+ssh $RELEASE_HOST 'cd /var/app/royhome-net ; sudo yarn run pm2:stop'
+ssh $RELEASE_HOST 'mv /var/app/royhome-net /var/app/royhome-net.$(date +%F_%T)'
+ssh $RELEASE_HOST 'git clone https://github.com/kwr760/royhome-net.git /var/app/royhome-net'
+ssh $RELEASE_HOST 'cd /var/app/royhome-net ; yarn install'
+ssh $RELEASE_HOST 'cd /var/app/royhome-net ; yarn run build:prod'
+ssh $RELEASE_HOST 'sudo -H certbot renew --standalone'
+ssh $RELEASE_HOST 'cd /var/app/royhome-net ; sudo yarn run pm2:start'
+ssh $RELEASE_HOST 'ls -la /var/app'
 
 # Don't forget to cleanup your agent after you're done using it if you're not on an ephemeral build server.
 ssh-agent -k
