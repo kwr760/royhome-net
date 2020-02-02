89c281b0623368411abb18c47ece67f20aea4dc6
Kevin Roy <kroy760@gmail.com>
2020-02-02 15:44:19 -0800

    WEB-22 Use the login learning example to improve tool bar (#63)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
-echo "Deploy the latest code"
+echo -e "\nDeploy the latest code"
 
 eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/private-key.enc -out .travis/private-key -d
@@ -6,13 +6,22 @@ chmod 600 .travis/private-key
 ssh-add .travis/private-key
 rm .travis/private-key
 
+echo -e "\nRemote:  stop existing server"
 ssh $RELEASE_HOST 'cd /var/app/royhome-net ; sudo yarn run pm2:stop'
+echo -e "\nRemote:  backup existing server"
 ssh $RELEASE_HOST 'mv /var/app/royhome-net /var/app/royhome-net.$(date +%F_%T)'
+echo -e "\nRemote:  git clone"
 ssh $RELEASE_HOST 'git clone https://github.com/kwr760/royhome-net.git /var/app/royhome-net'
+echo -e "\nRemote:  yarn install"
 ssh $RELEASE_HOST 'cd /var/app/royhome-net ; yarn install'
+echo -e "\nRemote:  build webpack"
 ssh $RELEASE_HOST 'cd /var/app/royhome-net ; yarn run build:prod'
+echo -e "\nRemote:  certbot renew"
 ssh $RELEASE_HOST 'sudo -H certbot renew --standalone'
+echo -e "\nRemote:  start new server"
 ssh $RELEASE_HOST 'cd /var/app/royhome-net ; sudo yarn run pm2:start'
+echo -e "\nRemote:  show new server"
+ssh $RELEASE_HOST 'cd /var/app/royhome-net ; sudo yarn run pm2:status'
 ssh $RELEASE_HOST 'ls -la /var/app'
 
 # Don't forget to cleanup your agent after you're done using it if you're not on an ephemeral build server.
