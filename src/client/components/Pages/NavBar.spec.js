1df86769b36fd8ba5b95dd9839256030e79949b1
kwr760 <kroy760@gmail.com>
2020-02-08 21:11:51 -0800

    WEB-45 Use Reactstrap

--- src/client/components/Pages/NavBar.spec.js
+++ src/client/components/Pages/NavBar.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import NavBar from './NavBar';
 import Context from '../Context';
 import initFontAwesome from '../initFontAwesome';
@@ -20,7 +20,7 @@ describe('client/components/Pages/NavBar', () => {
     };
 
     // Act
-    const { getByText } = render(
+    const { getByText, getByTestId } = render(
       <Router>
         <Context.Provider value={auth}>
           <NavBar />
@@ -34,6 +34,37 @@ describe('client/components/Pages/NavBar', () => {
     getByText(/Profile/);
     getByText(/Private/);
     getByText(/Courses/);
-    getByText(/Log Out/);
+    getByText(/Log out/);
+
+    fireEvent.click(getByText('Log out'));
+    expect(auth.logout).toHaveBeenCalled();
+
+    fireEvent.click(getByTestId('navbar-toggler'));
+  });
+  it('should render without authentication and role', () => {
+    // Arrange
+    const auth = {
+      isAuthenticated: jest.fn(() => false),
+      login: jest.fn(),
+      logout: jest.fn(),
+      userHasRole: jest.fn(() => true),
+    };
+
+    // Act
+    const { getByText } = render(
+      <Router>
+        <Context.Provider value={auth}>
+          <NavBar />
+        </Context.Provider>
+      </Router>,
+    );
+
+    // Assert
+    getByText(/Home/);
+    getByText(/Resume/);
+    getByText(/Log in/);
+
+    fireEvent.click(getByText('Log in'));
+    expect(auth.login).toHaveBeenCalled();
   });
 });
