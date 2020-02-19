65f17c1424bd2d2b6a90711d93e2b697d90dd5e6
Kevin Roy <kroy@expediagroup.com>
2020-02-19 07:19:10 -0800

    WEB-55 Code Clean up - unit tests

--- src/client/Components/Footer/Footer.js
+++ src/client/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer style={{ backgroundImage: 'linear-gradient(to bottom right, #1d3057, #828894)' }} className="p-3 text-center">
-    <div className="logo" />
+    <div className="logo" data-testid="footer-logo" />
   </footer>
 );
 
