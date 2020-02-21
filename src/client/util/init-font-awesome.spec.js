8a0cd8f2cc4c28eb40687a71feb78288abaaeeb3
Kevin Roy <kroy@expediagroup.com>
2020-02-21 07:18:11 -0800

    WEB-55 Code Clean up - unit tests - font-awesome

---
+++ src/client/util/init-font-awesome.spec.js
@@ -0,0 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import initFontAwesome from './init-font-awesome';
+
+jest.mock('@fortawesome/fontawesome-svg-core');
+
+describe('client/util/init-font-awesome', () => {
+  it('should load the library', () => {
+    // Arrange
+    const mockAdd = jest.fn();
+    library.add = mockAdd;
+
+    // Act
+    initFontAwesome();
+
+    // Assert
+    expect(mockAdd).toBeCalledTimes(5);
+  });
+});
