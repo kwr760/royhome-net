ede816a1136efff02b2492ebd71979b1dd675ffe
Kevin Roy <kroy@expediagroup.com>
2020-02-22 07:09:42 -0800

    WEB-55 Code Clean up - unit tests - add

--- src/util/history.spec.js
+++ src/util/history.spec.js
@@ -1,11 +1,9 @@
-describe('src/util/history', () => {
+import history from './history';
+
+describe('util/history', () => {
   it('is already tested', () => {
     // Arrange/Act
-    const requiresBrowser = 'history requires BrowserRouter';
-    const alreadyTested = 'Tested in index-web';
-
     // Assert
-    expect(requiresBrowser).toMatch(/browser/i);
-    expect(alreadyTested).toMatch(/index-web/);
+    expect(history).toBeInstanceOf(Object);
   });
 });
