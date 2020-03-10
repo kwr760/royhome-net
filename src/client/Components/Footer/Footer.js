5c94dc0a5912f7b952bf027fd5f640980a630119
kwr760 <kroy760@gmail.com>
2020-03-09 21:28:19 -0700

    WEB-53: scss working

--- src/client/Components/Footer/Footer.js
+++ src/client/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import { NavLink } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => (
-  <footer style={{ backgroundImage: 'linear-gradient(to bottom right, #1d3057, #828894)' }} className="p-3 text-center">
+  <footer className="p-3 text-center">
     <NavLink href="https://github.com/kwr760/royhome-net" target="_target" data-testid="footer-logo">
       <FontAwesomeIcon icon={['fab', 'github']} className="fa-2x" />
     </NavLink>
