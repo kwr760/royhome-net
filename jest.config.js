5945b4c8accf04859fb903d113a39ab2bf8ca44f
kwr760 <kroy760@gmail.com>
2020-04-08 18:32:35 -0700

    jest refactor

--- jest.config.js
+++ jest.config.js
@@ -1,74 +1,51 @@
+const commonIgnoreDirs = [
+  '<rootDir>/node_modules/',
+  '<rootDir>/build/',
+  '<rootDir>/dist/',
+];
+
 module.exports = {
   projects: [
     {
       name: 'browser',
       displayName: 'browser',
       testEnvironment: 'jsdom',
-      verbose: true,
-      moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$': '<rootDir>/src/mocks/file.js',
-        '\\.(css)$': 'identity-obj-proxy',
-      },
       testMatch: [
         '**/src/client/**/?(*.)(spec|test).js',
         '**/src/util/**/?(*.)(spec|test).js',
-      ],
-      modulePathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/build/',
-        '<rootDir>/dist/',
-      ],
-      testPathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/build/',
-        '<rootDir>/dist/',
-      ],
-      coveragePathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/build/',
-        '<rootDir>/dist/',
-        'webpack.config.babel.js',
-        '.eslintrc.js',
       ],
       transform: {
         '^.+\\.js$': 'babel-jest',
         '^.+\\.html$': 'html-loader-jest',
       },
+      moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$': '<rootDir>/src/mocks/file.js',
+        '\\.(css|scss)$': 'identity-obj-proxy',
+      },
+      modulePathIgnorePatterns: commonIgnoreDirs,
+      testPathIgnorePatterns: commonIgnoreDirs,
+      coveragePathIgnorePatterns: commonIgnoreDirs,
     },
     {
       name: 'server',
       displayName: 'server',
       testEnvironment: 'node',
-      verbose: true,
-      moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$': '<rootDir>/src/mocks/file.js',
-      },
       testMatch: [
         '**/src/server/**/?(*.)(spec|test).js',
         '**/src/config/**/?(*.)(spec|test).js',
       ],
-      modulePathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/build/',
-        '<rootDir>/dist/',
-      ],
-      testPathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/build/',
-        '<rootDir>/dist/',
-      ],
-      coveragePathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/build/',
-        '<rootDir>/dist/',
-        'webpack.config.babel.js',
-        '.eslintrc.js',
-      ],
       transform: {
         '^.+\\.js$': 'babel-jest',
       },
+      moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$': '<rootDir>/src/mocks/file.js',
+      },
+      modulePathIgnorePatterns: commonIgnoreDirs,
+      testPathIgnorePatterns: commonIgnoreDirs,
+      coveragePathIgnorePatterns: commonIgnoreDirs,
     },
   ],
+  verbose: true,
   collectCoverage: true,
   coverageDirectory: './build/coverage',
   coverageThreshold: {
