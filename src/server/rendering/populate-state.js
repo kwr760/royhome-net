0255551398c062ab2ba52285bbf602e45ed8c22b
kwr760 <kroy760@gmail.com>
2020-04-18 15:29:53 -0700

    WEB-77

--- src/server/rendering/populate-state.js
+++ src/server/rendering/populate-state.js
@@ -1,13 +1,14 @@
 // @flow
 
+import { matchPath } from 'react-router-dom';
 import type { StateType } from '../../client/store/types';
 import { COOKIE_JWT_PAYLOAD } from '../../util/auth0/constants';
-import { matchPath } from "react-router-dom";
 import routes from '../routes';
 import fetchInitialData from './fetch-initial-data';
 
 const populateState = (req: Request): StateType => {
   const activeRoute = routes.find((route) => matchPath(req.url, route)) || {};
+  // eslint-disable-next-line no-unused-vars
   const data = fetchInitialData(activeRoute.fetchData ? activeRoute.fetchData : {});
   const jwt = req.cookies[COOKIE_JWT_PAYLOAD] ? JSON.parse(req.cookies[COOKIE_JWT_PAYLOAD]) : {};
   const { exp = 0, user = {} } = jwt;
