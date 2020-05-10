a2b618edd95f1dd86a5730c74669986b2345aca0
kwr760 <kroy760@gmail.com>
2020-05-09 21:53:52 -0700

    WEB-79

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,6 @@
 echo -e "\nDeploy the latest code"
 
 eval "$(ssh-agent -s)"
-# openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/private-key.enc -out .travis/private-key -d
 chmod 600 .travis/private-key
 # openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/.env.enc -out .travis/.env -d
 # chmod 600 .travis/.env
