b9332350a716347feab09eb8937fd6b5cc458ff6
Kevin Roy <kroy760@gmail.com>
2020-05-09 21:44:23 -0700

    Web 79 (#110)
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-55 Code Clean up - unit tests - incomplete - need work
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    Co-authored-by: Kevin Roy <kroy@expediagroup.com>

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,8 +3,8 @@ echo -e "\nDeploy the latest code"
 eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/private-key.enc -out .travis/private-key -d
 chmod 600 .travis/private-key
-openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/.env.enc -out .travis/.env -d
-chmod 600 .travis/.env
+# openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/.env.enc -out .travis/.env -d
+# chmod 600 .travis/.env
 
 ssh-add .travis/private-key
 rm .travis/private-key
@@ -15,8 +15,8 @@ echo -e "\nRemote:  backup existing server"
 ssh $RELEASE_HOST 'mv /var/app/royhome-net /var/app/royhome-net.$(date +%F_%T)'
 echo -e "\nRemote:  git clone"
 ssh $RELEASE_HOST 'git clone https://github.com/kwr760/royhome-net.git /var/app/royhome-net'
-scp .travis/.env $RELEASE_HOST:/var/app/royhome-net
-rm .travis/.env
+scp .env $RELEASE_HOST:/var/app/royhome-net
+rm .env
 echo -e "\nRemote:  yarn install"
 ssh $RELEASE_HOST 'cd /var/app/royhome-net ; yarn install'
 echo -e "\nRemote:  build webpack"
