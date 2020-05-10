d9ba7d1330caff687bcbaeed3f392a769ac2cc9f
Kevin Roy <kroy760@gmail.com>
2020-05-09 21:08:41 -0700

    Web 79 (#107)
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-55 Code Clean up - unit tests - incomplete - need work
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    * WEB-79
    
    Co-authored-by: Kevin Roy <kroy@expediagroup.com>

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,6 +2,7 @@ echo -e "\nDeploy the latest code"
 
 eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/private-key.enc -out .travis/private-key -d
+openssl aes-256-cbc -K $encrypted_85fdd383440b_key -iv $encrypted_85fdd383440b_iv -in .travis/.env.enc -out .env -d
 chmod 600 .travis/private-key
 ssh-add .travis/private-key
 rm .travis/private-key
