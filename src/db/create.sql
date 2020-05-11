95d01aa179d40e0f934fca8406da8b8d55243a44
kwr760 <kroy760@gmail.com>
2020-05-10 22:15:05 -0700

    WEB-79

--- src/db/create.sql
+++ src/db/create.sql
@@ -21,7 +21,7 @@ CREATE SEQUENCE resume_address_id_seq;
 CREATE TABLE resume_address
 (
   id integer NOT NULL DEFAULT nextval('resume_address_id_seq'::regclass) PRIMARY KEY,
-  user_id integer REFERENCES resume_owner (resume_owner_id),
+  user_id integer REFERENCES resume_owner (id),
   address character varying(50) NOT NULL
 );
 ALTER TABLE resume_owner OWNER TO server;
@@ -30,7 +30,7 @@ CREATE SEQUENCE resume_contact_id_seq;
 CREATE TABLE resume_contact
 (
   id integer NOT NULL DEFAULT nextval('resume_contact_id_seq'::regclass) PRIMARY KEY,
-  user_id integer REFERENCES resume_owner (resume_owner_id),
+  user_id integer REFERENCES resume_owner (id),
   phone character varying(20) NOT NULL,
   display_phone boolean NOT NULL,
   email character varying(50) NOT NULL
