654bab0b0805f2237050d15efa7c7739fdbacc8b
kwr760 <kroy760@gmail.com>
2020-05-11 17:28:52 -0700

    WEB-79

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
       verbose: true,
       testMatch: [
         // '**/src/client/**/?(*.)(spec|test).js',
-        '**/src/util/**/?(*.)(spec|test).js',
+        // '**/src/util/**/?(*.)(spec|test).js',
       ],
       transform: {
         '^.+\\.js$': 'babel-jest',
@@ -34,7 +34,7 @@ module.exports = {
       verbose: true,
       testMatch: [
         '**/src/server/**/?(*.)(spec|test).js',
-        '**/src/config/**/?(*.)(spec|test).js',
+        // '**/src/config/**/?(*.)(spec|test).js',
       ],
       transform: {
         '^.+\\.js$': 'babel-jest',
