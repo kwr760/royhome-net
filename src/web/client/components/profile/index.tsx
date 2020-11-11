1b5765a3d14815d3d49e80c056c47e4ee2143b44
kwr760 <kroy760@gmail.com>
2020-11-10 21:22:17 -0800

    WEB-103: profile

--- src/web/client/components/profile/index.tsx
+++ src/web/client/components/profile/index.tsx
@@ -1,6 +1,6 @@
 import { isEmpty } from 'lodash';
 import React, { FunctionComponent } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Grid } from '@material-ui/core';
 
 import { useSelector } from 'react-redux';
 import Loading from '../page/loading';
@@ -16,26 +16,26 @@ const Profile: FunctionComponent = () => {
     displayLoading ? <Loading />
       : (
         <Container className="mb-5">
-          <Row className="align-items-center profile-header mb-5 text-center text-md-left">
-            <Col md={2}>
+          <Grid container className="align-items-center profile-header mb-5 text-center text-md-left">
+            <Grid item md={2}>
               <img
                 src={user.picture}
                 alt="Profile"
                 className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
               />
-            </Col>
-            <Col md>
+            </Grid>
+            <Grid item md>
               <h2>{user.name}</h2>
               <p className="lead text-muted">{user.email}</p>
-            </Col>
-          </Row>
-          <Row>
+            </Grid>
+          </Grid>
+          <Grid container>
             <pre className="rounded">
               <code>
                 {JSON.stringify(user, null, 2)}
               </code>
             </pre>
-          </Row>
+          </Grid>
         </Container>
       )
   );
