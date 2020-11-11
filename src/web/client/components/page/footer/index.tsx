993f766d84565c2d241b871dc7334d6866e2e51a
kwr760 <kroy760@gmail.com>
2020-11-10 21:22:37 -0800

    WEB-103: footer

--- src/web/client/components/page/footer/index.tsx
+++ src/web/client/components/page/footer/index.tsx
@@ -1,18 +1,16 @@
 import React, { FunctionComponent } from 'react';
-import { NavLink } from 'reactstrap';
+import { Link } from '@material-ui/core';
 import { VscGithub } from 'react-icons/vsc';
 
 const Footer: FunctionComponent = () => (
   <footer className="p-3 text-center">
-    <NavLink
+    <Link
       href="https://github.com/kwr760/royhome-net"
-      alt="github"
       target="_target"
-      data-testid="footer-logo"
     >
       <VscGithub className="fa-2x" />
       <span className="sr-only">Link to github</span>
-    </NavLink>
+    </Link>
   </footer>
 );
 
