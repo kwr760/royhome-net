02513d23e5d02405f1bad3ec2daab8aa48a01527
kwr760 <kroy760@gmail.com>
2020-12-02 21:42:02 -0800

    WEB-119

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,28 +13,25 @@ ssh-add private-key
 echo -e "\nTravis:  openssl rm"
 rm private-key
 
-echo -e "\nRemote:  remove old release"
-ssh $RELEASE_HOST 'rm -rf /var/app/royhome-net.20*'
-echo -e "\nRemote:  stop existing server"
-ssh $RELEASE_HOST 'cd /var/app/royhome-net ; sudo yarn run pm2:stop'
-echo -e "\nRemote:  backup existing server"
-ssh $RELEASE_HOST 'mv /var/app/royhome-net /var/app/royhome-net.$(date +%F_%T)'
-echo -e "\nRemote:  git clone"
-ssh $RELEASE_HOST 'git clone https://github.com/kwr760/royhome-net.git /var/app/royhome-net'
+echo -e "\nRemote:  copy new code to stage"
+ssh $RELEASE_HOST 'git clone https://github.com/kwr760/royhome-net.git /var/app/royhome-net.stage'
 echo -e "\nRemote:  scp env"
 scp .env $RELEASE_HOST:/var/app/royhome-net
-echo -e "\nRemote:  yarn install"
-ssh $RELEASE_HOST 'cd /var/app/royhome-net ; yarn install --production'
-echo -e "\nRemote:  build webpack"
-ssh $RELEASE_HOST 'cd /var/app/royhome-net ; node --version'
-ssh $RELEASE_HOST 'cd /var/app/royhome-net ; yarn run build:prod'
+echo -e "\nRemote:  docker-compose build"
+ssh $RELEASE_HOST 'cd /var/app/royhome-net.stage ; RELEASE=prod docker-compose build'
+echo -e "\nRemote:  stop existing server"
+ssh $RELEASE_HOST 'cd /var/app/royhome-net ; RELEASE=prod docker-compose down'
+echo -e "\nRemote:  remove old release"
+ssh $RELEASE_HOST 'rm -rf /var/app/royhome-net.rollback'
+echo -e "\nRemote:  backup existing server"
+ssh $RELEASE_HOST 'mv /var/app/royhome-net /var/app/royhome-net.rollback'
+echo -e "\nRemote:  promote staging"
+ssh $RELEASE_HOST 'mv /var/app/royhome-net.stage /var/app/royhome-net'
+echo -e "\nRemote:  start new server"
+ssh $RELEASE_HOST 'cd /var/app/royhome-net ; RELEASE=prod docker-compose up -d'
+
 echo -e "\nRemote:  certbot renew"
 ssh $RELEASE_HOST 'sudo -H certbot renew --standalone'
-echo -e "\nRemote:  start new server"
-ssh $RELEASE_HOST 'cd /var/app/royhome-net ; sudo yarn run pm2:start'
-echo -e "\nRemote:  show new server"
-ssh $RELEASE_HOST 'cd /var/app/royhome-net ; sudo yarn run pm2:status'
-ssh $RELEASE_HOST 'ls -la /var/app'
 
 # Don't forget to cleanup your agent after you're done using it if you're not on an ephemeral build server.
 ssh-agent -k
